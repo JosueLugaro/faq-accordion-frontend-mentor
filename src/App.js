3a25061b1caec1eeb55c4cb3ceed78ec91ded2ad
Josue Lugaro <josuelugaro15@gmail.com>
2023-12-22 13:40:57 -0600

    Removed unnecessary variable and console logs from App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import Card from './components/Card'
 import './App.css';
 
 function App() {
-  const desktopBackground = process.env.PUBLIC_URL + "/images/background-pattern-desktop.svg"
-  console.log(process.env.PUBLIC_URL, 'PUBLIC URL')
-  console.log(desktopBackground, 'DESKTOP')
-
   return (
     <div className="bg-[#F9F0FF] h-screen">
       <div className="bg-mobile-background sm:bg-desktop-background w-screen h-[232px] sm:h-[320px]"/>
